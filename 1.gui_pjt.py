fc8a1c22c4d7dfd1d4192d31c9243fb7f7d9918f
seoyeon <hskyblue22@naver.com>
2020-10-01 21:38:33 +0900

    gui programming

--- 1.gui_pjt.py
+++ 1.gui_pjt.py
@@ -1,23 +1,103 @@
 from tkinter import *
+'''
+프레임 사이 간격이 너무 좁을때 
+==> frame.pack(padx= 5, pady= 5) 넣어보자
+==> 버튼에서 Button(master, padx= 5, pady= 5 ): 버튼 내 text와 간격
+==> 버튼에서 Button.pack(padx= 5, pady= 5 ): 버튼과 프레임 사이 간격
+'''
 
-root = Tk()                           # Tk 클래스 객체
-root.title("Alarm")
-root.geometry("480x640+900+100")      # 소문자 x써야함 
+root = Tk()                   # Tk 클래스 객체
+root.title("사진 합치기")                    
 
-root.resizable(True,True)            # x,y 창 크기 변경
+# 파일 프레임 (파일추가, 파일삭제 버튼 만들기)
+frame_file = Frame(root)
+frame_file.pack(fill="x", padx= 5, pady= 5)     # x로 펼치기
 
-label1 = Label(root, text="좋은 아침이에요:)")
-label1.pack()
+btn_addfile = Button(frame_file, padx= 5, pady= 5, width = 12, text= "파일추가")  
+btn_addfile.pack(side="left")
 
-wall = PhotoImage(file = "best.png")
-wall_label = Label(root, image=wall)
-wall_label.place(x=-2,y=120)         # x=0, y=0으로 하고 이미지 봐서 조정함
+btn_addfile = Button(frame_file, padx= 5, pady= 5, width = 12,text= "파일추가") 
+btn_addfile.pack(side="right")
+
+# 리스트 프레임
+frame_list = Frame(root)
+frame_list.pack(fill= "both", padx= 5, pady= 5)         # frame도 .pack(fill= "both")로 꽉 채울수 있군
+
+scroll = Scrollbar(frame_list)
+scroll.pack(side= "right", fill= "y")
+
+list_file = Listbox(frame_list, selectmode = "extended", height =15, yscrollcommand = scroll.set)
+list_file.pack(side = "left", fill= "both", expand = True)
+
+scroll.config(command= list_file.yview)
+
+# 저장경로 프레임 만들기
+frame_path = LabelFrame(root, text="저장경로")
+frame_path.pack(fill="x", padx= 5, pady= 5, ipady=5)
+
+txt_path = Entry(frame_path)                              # enter 불가능. 한줄로 입력받는 아이디,비번 같은 경우
+txt_path.pack(side= "left",fill= "x", expand= True, ipady= 4, padx= 5, pady= 5)     # fill= "x", expand=True 안써도 똑같은데 왜 쓰지? 
+                                                                  # ipady = 4 : 높이 변경
+btn_search = Button(frame_path, width=10, text="찾아보기")
+btn_search.pack(side= "right", padx= 5, pady= 5)
 
 
-def change():
-    label1.config(text="좋은 시작이에요♥")
+# 옵션 프레임 만들기
+import tkinter.ttk as ttk
+frame_option = LabelFrame(root, text="옵션")
+frame_option.pack(padx= 5, pady= 5, ipady=5)
 
-btn1 = Button(root, text = "클릭", command=change)
-btn1.pack()
+# 너비 레이블
+lbl_width = Label(frame_option,text="가로넓이",width=8)
+lbl_width.pack(side= "left", padx= 5, pady= 5)
 
+# 너비 콤보박스
+v_width= ["원본유지", "1024", "800", "600"]
+combo_width= ttk.Combobox(frame_option, values= v_width, state="readonly", width=8)
+combo_width.current(0)
+combo_width.pack(side= "left", padx= 5, pady= 5)
+
+# 간격 레이블
+lbl_gap = Label(frame_option,text="간격",width=8)
+lbl_gap.pack(side= "left", padx= 5, pady= 5)
+
+# 간격 콤보박스
+v_gap= ["넓음","보통","좁음","없음"]
+combo_gap= ttk.Combobox(frame_option, values= v_gap, state="readonly", width=8)
+combo_gap.current(1)
+combo_gap.pack(side= "left", padx= 5, pady= 5)
+
+# 포맷 레이블
+frame_lbl = Label(frame_option,text="포맷",width=8)
+frame_lbl.pack(side= "left", padx= 5, pady= 5)
+
+# 포맷 콤보박스
+v_format= ["PNG","JPG","GMP"]
+combo_format= ttk.Combobox(frame_option, values= v_format, state="readonly", width=8)
+combo_format.current(0)
+combo_format.pack(side= "left", padx= 5, pady= 5)
+
+
+# 진행상황 progress bar
+
+frame_progress = LabelFrame(root, text= "진행상황")
+frame_progress.pack(fill="x", padx= 5, pady= 5)
+
+p_var = DoubleVar()
+progress_var = ttk.Progressbar(frame_progress, maximum = 100, variable= p_var)
+progress_var.pack(fill="x", padx= 5, pady= 5)
+
+
+# 실행 프레임 만들기
+frame_run = Frame(root)
+frame_run.pack(fill="x", padx= 5, pady= 5)
+
+btn_close = Button(frame_run, padx= 5, pady= 5, width = 12,text="닫기", command= root.quit)
+btn_close.pack(side="right", padx= 5, pady= 5)
+
+btn_run = Button(frame_run, padx= 5, pady= 5, width = 12, text="시작")
+btn_run.pack(side= "right", padx= 5, pady= 5)
+
+
+root.resizable(False,False)  
 root.mainloop()
--- memopad.py
+++ memopad.py
@@ -1,31 +1,66 @@
+import os
 from tkinter import *
 
 root = Tk()
 root.title("제목 없음 - Windows 메모장")
 root.geometry("680x480")
-root.resizable(True,True)
+# root.resizable(True,True)       지워도 크기조절 자유롭게 가능
 
-frame = Frame(root)
-frame.pack()
-scroll= Scrollbar(frame)
+# frame = Frame(root)
+# frame.pack()
+'''
+txt말고 다른 위젯이 없으므로 스크롤바를 바로 root에 넣기!
+'''
+scroll= Scrollbar(root)
 scroll.pack(side="right",fill="y")
 
-txt= Text(frame,width=680,height=480,yscrollcommand=scroll.set)
-txt.pack() 
-
+# txt= Text(frame,width=680,height=480,yscrollcommand=scroll.set)
+txt = Text(root, yscrollcommand = scroll.set)       # 전체화면에 꽉차게 만들기
+txt.pack(fill="both", expand=True) 
+''' 
+fill: 요구되었지만 사용되지 않은 공간에 대해 위젯이 늘어남
+expand: 요구되지 않은 공간을 사용하고 싶을때 사용. 현재틀에서 요구할 수 있는 모든 공간 요구
+set: 위젯에 스크롤바 적용(스크롤바 연결)
+'''
 scroll.config(command = txt.yview)
 
 
+filename= "mynote.txt"
+
+def openfile():     # filename 이라는 파일이 있는지 없는지 확인하고 열어줘야함!! (내가 빼뜨린 부분)
+    if os.path.isfile(filename):
+        with open(filename, "r", encoding = "utf8") as file:
+            txt.delete("1.0",END)      # 메모장 본문에 있던 내용을 지워주고 file에 있는 내용 읽어야함!
+            txt.insert(END, file.read())     # 파일내용을 본문에 입력
+'''
+os.path.isfile(filename))  ==> filename이라는 파일 있으면 True, 없으면 False
+with open() as ㅇㅇ:  ==> with open뒤에 : 를 붙여줌
+txt.insert(END, file.read())  ==> END: 끝에 붙여줌  /  file.read(): 파일 통째로 읽기
+txt.delete(index, index)  ==> 앞 내용 지워주기 index는 범위!
+'''
+
 def savefile():
-    file_note = open("mynote.txt", "w", encoding = "utf8")   # file_score : 파일 객체를 저장하는 변수의 이름
-    print(txt.get(), file = file_note)
-    file_note.close()
+    with open(filename, "w", encoding = "utf8") as file:
+        file.write(txt.get("1.0",END))
+
+
+# def openfile():
+#     file_note = open("mynote.txt", "w", encoding = "utf8")   # file_score : 파일 객체를 저장하는 변수의 이름
+#     print(txt.get("1.0",END), file = file_note)
+#     file_note.close()
+
+# def savefile():
+#     file_note = open("mynote.txt", "r", encoding = "utf8")
+#     line = file_note.readline()
+#     txt.insert(END,line)
+     
+#     file_note.close() 
 
 
 menubar= Menu(root)
 
 menu1= Menu(menubar,tearoff = 0)
-menu1.add_command(label="열기")
+menu1.add_command(label="열기",command=openfile)
 menu1.add_command(label="저장",command= savefile)
 menu1.add_separator()
 menu1.add_command(label="끝내기",command=root.quit)
