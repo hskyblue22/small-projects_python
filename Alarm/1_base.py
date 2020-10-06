c5121b4b4b6d156bf63914a1ef62c579608995e1
seoyeon <hskyblue22@naver.com>
2020-10-06 23:57:50 +0900

    making alarm

--- Alarm/1_base.py
+++ Alarm/1_base.py
@@ -1,6 +1,7 @@
 from tkinter import *
 import datetime
 import time
+import winsound
 # from datetime import datetime      # datetime 라이브러리_now함수로 현재 날짜,시간 호출 *
 
 def update_timelabel():
@@ -15,13 +16,45 @@ def update_timelabel():
 
 root = Tk()
 root.title("Alarm Clock")
+root.geometry("430x300")
+
+frame_now = Frame(root)
+frame_now.pack( padx= 5, pady= 5)
 
 # timelabel 생성하기 & update_timelabel함수로 값 변경
-timelabel1 = Label(root, text="", font=("Arial", 20))
+timelabel1 = Label(frame_now, text="", font=("Arial", 20))
 timelabel1.pack()
-timelabel2 = Label(root, text="", font=("Arial", 30))
+timelabel2 = Label(frame_now, text="", font=("Arial", 27))
 timelabel2.pack()
-# update_timelabel()
+update_timelabel()
+
+
+frame_set_alarm = LabelFrame(root, text="Alarm")
+frame_set_alarm.pack( fill= "x", padx= 5, pady= 5, ipady= 80)
+
+# 입력한 시간 초기화 할때 필요한 변수
+hour = StringVar()
+min = StringVar()
+sec = StringVar()
+
+# 알람 설정한 시간(사용자 입력)
+hourTime= Entry(frame_set_alarm, textvariable = hour, width=8)
+# hourTime.pack(side="left", ipady=7,padx=10,pady=10)
+hourTime.place(x=90,y=50,ipdy=7)
+minTime= Entry(frame_set_alarm,textvariable = min,width=8)
+# minTime.pack(side="left", ipady=7,padx=10,pady=10)
+minTime.place(x=180,y=50)
+secTime = Entry(frame_set_alarm,textvariable = sec,width=8)
+# secTime.pack(side="left", ipady=7,padx=10,pady=10)
+secTime.place(x=270,y=50)
+
+# alarm_set = Button(frame_set_alarm, text= "Set Alarm")
+# alarm_set.pack(padx=5, pady=5)
+
+
+
+
+
 root.mainloop()
 
 
