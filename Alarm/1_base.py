7fde0d2c0999d7a8200e537c7316c3c58bda7c7f
seoyeon <hskyblue22@naver.com>
2020-10-16 20:36:09 +0900

    리스트에 값 넣는 방법 생각하는 중

--- Alarm/1_base.py
+++ Alarm/1_base.py
@@ -1,23 +1,23 @@
 from tkinter import *
 import datetime
 import time
-import winsound
 import tkinter.messagebox as msgbox
 import threading
 from pygame import mixer 
-import playsound
+import playsound                                                                                                                        
  
-
 
 # mixer.init()
 
+# alarm thread 실행 
 def th():
     t1= threading.Thread(target=set_alarm, args=())
     t1.start()
 
+# 현재시간 업데이트 하기
 def update_timelabel():
     # 현재 날짜, 시각
-    current_date = time.strftime("%Y년 %m월 %d일")
+    current_date = time.strftime("%Y/%m/%d")
     current_time = time.strftime("%H:%M:%S")
     # timelabel 값 바꿔주기
     timelabel1.configure(text=current_date)
@@ -25,72 +25,67 @@ def update_timelabel():
     # update_timelabel()함수를 1초마다 호출
     root.after(1000, update_timelabel)
 
+# 레이블 값 바꾸기
+def label_change():
+    if 0<=int(hourTime.get())<=24 and 0<=int(minTime.get())<=60:
+        label_var.set("Alarm has been set :)")          # stringvar에는 configure속성이 없다.
+    else:
+        label_var.set("Please check the time again.")
+
 # set버튼 클릭
 def set_alarm():
     # hour 입력 확인
-    # h = int(hourTime.get())
-    if hourTime.index("end") ==0:
-        msgbox.showwarning("경고","시간(hour)을 입력해주세요")         
+    h = hourTime.get()
+    # if hourTime.index("end") ==0:       '빈 칸이다' 이렇게도 표현 가능
+    if len(h) == 0:
+        msgbox.showwarning("warning","Please enter the hour.")         
     elif 0<= int(hourTime.get()) <=24:
-        # hour.set( Done")
         pass               # return : 전체 함수 빠져나감(set_alarm빠져나가기 때문에 밑에 분,초는 검사가 안됨)
     else:
-        msgbox.showwarning("경고", "시간(hour)을 정확히 입력해주세요")   
+        msgbox.showwarning("warning", "Please check the hour again.")   
 
     # min 입력 확인
     m = minTime.get()
     if len(m) ==0:
-        msgbox.showwarning("경고","분(min)을 입력해주세요")
+        msgbox.showwarning("warning","Please enter the minute.")
     elif 0 <= int(m) <= 60 :
-        # min.set(" Done")
         pass
     else:
-        msgbox.showwarning("경고", "분(min)을 정확히 입력해주세요")
+        msgbox.showwarning("warning", "please check the minute again.")
 
-    # sec 입력 확인
-    s = secTime.get()
-    if len(s) == 0:
-        msgbox.showwarning("경고","초(sec)를 입력해주세요")
-    elif 0<= int(s)<=60 :
-        # sec.set(" Done")
-        pass
-    else:
-        msgbox.showwarning("경고", "초(sec)를 정확히 입력해주세요")
+    label_change()
 
-    if 0<=int(hourTime.get())<=24 and 0<=int(m)<=60 and 0<=int(s)<=60:
-        label_var.set("Alarm has been set :)")
-    else:
-        label_var.set("Please check the time again.")
-
-    user_hour= int(hourTime.get())
-    user_min= int(minTime.get())
+    user_hour= int(h)
+    user_min= int(m)
     now_hour = datetime.datetime.now().hour
     now_min= datetime.datetime.now().minute
 
+    # 알람시간과 현재시간 비교하기
     while user_hour != now_hour or user_min != now_min :
+        # now_hour를 현재시간으로 계속 바꿔줌 (real time으로 바껴야지 알람시간과 비교가능!)
         now_hour = datetime.datetime.now().hour
         now_min= datetime.datetime.now().minute
-
 
     if user_hour == now_hour and user_min == now_min : 
         # mixer.music.load('sea_waves.wav')
 	    # mixer.music.play()
-        
-        msg= msgbox.showinfo("알림","It is time")  
+
+        msg= msgbox.showinfo("Alarm Alarm","It is time")  
         playsound.playsound('sea_waves.wav', True)
        # 재생시간 공부 필요
 
 
 root = Tk()
+# root.configure(bg="black")
 root.title("Alarm Clock")
-root.geometry("430x400")
+root.geometry("430x600")
 
 # frame_시간 설정
 frame_now = Frame(root)
 frame_now.pack( padx= 5, pady= 5)
 
 # timelabel 생성하기 & update_timelabel함수로 값 변경
-timelabel1 = Label(frame_now, text="", font=("Arial", 20, "bold"))
+timelabel1 = Label(frame_now, text="", font=("Arial", 26, "bold"))
 timelabel1.pack()
 timelabel2 = Label(frame_now, text="", font=("Arial", 27, "bold"))
 timelabel2.pack()
@@ -98,47 +93,52 @@ update_timelabel()
 
 # 알람시간 설정 프레임
 frame_set_alarm = LabelFrame(root, text="Alarm")
-frame_set_alarm.pack( fill= "x", padx= 5, pady= 5, ipady= 110)
-
+frame_set_alarm.pack( fill= "x", padx= 5, pady= 5, ipady= 100)
+# 알람시간 입력칸 위 레이블(hour,min,second)
 label_hour= Label(frame_set_alarm, text="hour", font=("Arial",17), fg="mediumorchid2")
-label_hour.place(x=96, y=10)
-
+label_hour.place(x=96, y=0)
 label_min= Label(frame_set_alarm, text="min", font=("Arial",17), fg="mediumorchid2")
-label_min.place(x=190, y=10)
-
+label_min.place(x=190, y=0)
 label_sec= Label(frame_set_alarm, text="sec", font=("Arial",17), fg="mediumorchid2")
-label_sec.place(x=279, y=10)
+label_sec.place(x=279, y=0)
 
 
 # 입력한 시간 초기화 할때 필요한 변수
 hour = StringVar()
 min = StringVar()
-sec = StringVar()
 
-# 알람 설정한 시간(사용자 입력)
+# 알람시간 입력칸
 hourTime= Entry(frame_set_alarm, textvariable= hour, width=8)
-hourTime.place(x=90,y=43,height=35)
+hourTime.place(x=90,y=39,height=34)
 
 minTime= Entry(frame_set_alarm, textvariable = min ,width=8)
-minTime.place(x=180,y=43,height=35)
+minTime.place(x=180,y=39,height=34)
 
-secTime = Entry(frame_set_alarm, textvariable= sec, width=8)
-secTime.place(x=270,y=43,height=35)
+secTime = Entry(frame_set_alarm, width=8)
+secTime.place(x=270,y=39,height=34)
+secTime.insert(END,"00")
+secTime.configure(state="readonly")
 
-# alarm_set = Button(frame_set_alarm, text= "Set Alarm")
-# alarm_set.pack(padx=5, pady=5)
-
-# 24시간 형식 요청 레이블
+# 24시간형식 입력 요청 레이블
 label_var= StringVar()
 label_var.set("Enter time in 24 hour format")
 
 label_format= Label(frame_set_alarm, textvariable= label_var, font=("Arial",13), bg="palevioletred1",padx=2,pady=2)
-label_format.place(x= 104, y=100)
+label_format.place(x= 104, y=92)
 
 # set 버튼
 btn_set = Button(frame_set_alarm, text="Set",font=("Arial",15),padx=4,pady=2, width=4, command=th)
-btn_set.place(x=180, y= 140)
+btn_set.place(x=180, y= 130)
+# alarm_set = Button(frame_set_alarm, text= "Set Alarm")
+# alarm_set.pack(padx=5, pady=5)
 
+
+frame_list = Frame(root)
+frame_list.pack( fill= "x", padx= 5, pady= 30, ipady= 100)
+
+# 리스트박스
+list_alarm = Listbox(frame_list)
+list_alarm.pack()
 
 
 
--- Alarm/2_thread.py
+++ Alarm/2_thread.py
@@ -15,11 +15,15 @@ print("Hello")
 print("Hello")
 
 
-##### t.join() 없을 때
+##### t.join() 없을 때 : 내가 만든 thread와 main thread 간 번갈아서 실행된다.
 # Hi I'm thread1. Going to sleep for 5 seconds
 # Hello
 # Hello
 # thread1 has woke up from sleep
 
-##### t.join() 있을 때
-#
+##### t.join() 있을 때: 내가 만든 thread와 main thread 간 번갈아서 실행되지 않고 한 thread가 끝나고 
+# 다음 thread가 실행된다.
+# Hi I'm thread1. Going to sleep for 5 seconds
+# thread1 has woke up from sleep
+# Hello
+# Hello
